a4a0a3f963898b3d52293388850d32ab2d3de447
LeandroBP <leandrobatistapereira98@gmail.com>
2022-11-16 01:14:58 -0300

    Removendo contato e adicionando head

--- recipes/views.py
+++ recipes/views.py
@@ -1,21 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # HTTP REQUEST
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Leandro Batista',
     })
     # HTTP RESPONSE
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html', context={
-        'name': 'Python',
-
-    })
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
